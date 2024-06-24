daefb0e8cf6ca419c98b50cab1d150e8f135717d
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-06-24 23:10:17 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 380 ms, Memory: 129204 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,25 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
 #누적합 계산
-for i in range(N):
+dp = [[0] * (N + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
   line_sum = 0
-  for j in range(N):
-    line_sum += board[i][j]
-    if i == 0:
-      board[i][j] = line_sum
-    elif i > 0:
-      board[i][j] = line_sum + board[i - 1][j]
+  for j in range(1, N + 1):
+    line_sum += board[i - 1][j - 1]
+    dp[i][j] = line_sum + dp[i - 1][j]
 
 for _ in range(M):
   x1, y1, x2, y2 = map(int, input().split()) #x1<=x2, y1<=y2
-  x1, y1, x2, y2 = x1 - 1, y1 - 1, x2 - 1, y2 - 1
-  if x1 > 0 and y1 > 0:
-    sum = board[x2][y2] - (board[x1 - 1][y2]) - (board[x2][y1 - 1] - board[x1 - 1][y1 - 1])
-  elif x1 > 0 and y1 == 0:
-    sum = board[x2][y2] - (board[x1 - 1][y2])
-  elif x1 == 0 and y1 >0:
-    sum = board[x2][y2] - board[x2][y1 - 1]
-  elif x1 == 0 and y1 == 0:
-    sum = board[x2][y2]
+  sum = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]
   print(sum)
