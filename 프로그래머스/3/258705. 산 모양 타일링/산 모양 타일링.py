e3ce0659fd9a29d4298914a6fad5bea298b870a2
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2024-09-14 19:50:20 +0900

    [level 3] Title: 산 모양 타일링, Time: 115.26 ms, Memory: 19.7 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,23 +1,22 @@
 def solution(n, tops):
-    dp1 = [0] * (n) # dp1[i] = n번째 윗변에 삼각형이 사용된 경우 채우는 경우의 수
-    dp2 = [0] * (n) # dp2[i] = n번째 윗변에 첫 번째 마름모가 사용된 경우 채우는 경우의 수
-    dp3 = [0] * (n) # dp3[i] = n번째 윗변에 두 번째 ...
-    dp4 = [0] * (n) # dp4[i] = n번째 윗변에 세 번째 ...
+    dp1 = [0] * n # 삼각형
+    dp2 = [0] * n # 1번 마름모
+    dp3 = [0] * n # 2번 마름모
+    dp4 = [0] * n # 3번 마름모
     
     dp1[0] = 1
     dp2[0] = 1
     dp3[0] = 1
     if tops[0] == 1:
         dp4[0] = 1
+    
     for i in range(1, n):
-        dp1[i] = (dp1[i - 1] + dp2[i - 1] + dp3[i - 1] + dp4[i - 1]) % 10007
-        dp2[i] = (dp1[i - 1] + dp2[i - 1]) % 10007
-        if tops[i - 1] == 1:
-            dp2[i] += (dp4[i - 1]) % 10007
+        dp2[i] = (dp1[i - 1] + dp2[i - 1] + dp4[i - 1]) % 10007
         dp3[i] = (dp1[i - 1] + dp2[i - 1] + dp3[i - 1] + dp4[i - 1]) % 10007
         if tops[i] == 1:
-            dp4[i] = (dp1[i - 1] + dp2[i - 1] + dp3[i - 1] + dp4[i - 1]) % 10007
+            dp4[i] = dp3[i]
         else:
             dp4[i] = 0
+        dp1[i] = dp3[i]
     answer = (dp1[n - 1] + dp2[n - 1] + dp3[n - 1] + dp4[n - 1]) % 10007
     return answer
