62b71f58dd38ac828d9d7ae932a9cd730e4cfa5e
Choi Jeongmin <89333351+likerhythm@users.noreply.github.com>
2025-02-19 09:44:46 +0900

    [Silver II] Title: 연속합, Time: 244 ms, Memory: 25720 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        for (int i=1; i<n; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        }
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
