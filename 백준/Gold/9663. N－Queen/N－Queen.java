bfb738588fb4bde0729ab062d93462a0d54c3948
Choi Jeongmin <wjdalsdl0412@gmail.com>
2025-05-02 09:36:38 +0900

    [Gold IV] Title: N-Queen, Time: 5500 ms, Memory: 17520 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,89 +1,82 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     static int N;
-    static int[][] board;
-    static int answer = 0;
-    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static int[] queenX; // queenX[y] : y행에 위치한 퀸의 행 좌표
+    static int answer;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        queenX = new int[N];
+        Arrays.fill(queenX, -1);
+        f(0);
 
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(bf.readLine());
-        board = new int[N][N];
-
-        int[] queenRow = new int[N];
-        for (int i=0; i<N; i++) {
-            queenRow[i] = -1;
-        }
-
-        f(0, queenRow);
         System.out.println(answer);
     }
 
-    private static void f(int line, int[] queenRow) {
-        if (line == N) {
+    static void f(int x) {
+        if (x == N) {
             answer++;
             return;
         }
 
-        for (int i=0; i<N; i++) {
-            if (hasRow(queenRow, i)) {
+        for (int y=0; y<N; y++) {
+            if (queenRange(x, y)) { // 다른 퀸에게 영향을 받는 경우
                 continue;
             }
-            queenRow[line] = i;
-            if (!check(queenRow, line + 1)) { // 대각선에 존재하는 경우
-                queenRow[line] = -1;
-                continue;
-            }
-            f(line + 1, queenRow);
-            queenRow[line] = -1;
+
+            queenX[y] = x;
+            f(x + 1);
+            queenX[y] = -1;
         }
     }
 
-    private static boolean hasRow(int[] row, int i) {
-        for (int r : row) {
-            if (r == i) {
-                return true;
+    static boolean queenRange(int x, int y) {
+        if (queenX[y] >= 0) { // y열에 퀸이 있는 경우
+            return true;
+        }
+
+        int rate = 1;
+        boolean found = false;
+        while (true) {
+            if (findQueen(x, y, rate++)) {
+                found = true;
+                break;
+            }
+
+            if (rate == N) {
+                break;
             }
         }
+
+        return found;
+    }
+
+    static boolean findQueen(int x, int y, int rate) {
+        boolean found = false;
+        if (inRange(x - rate, y - rate) && queenX[y - rate] == x - rate) { // 왼쪽 위 칸이 범위 안에 있고 퀸이 있는 경우
+            return true;
+        }
+
+        if (inRange(x - rate, y + rate) && queenX[y + rate] == x - rate) { // 오른쪽 위 대각선
+            return true;
+        }
+
+        if (inRange(x + rate, y - rate) && queenX[y - rate] == x + rate) {
+            return true;
+        }
+
+        if (inRange(x + rate , y + rate) && queenX[y + rate] == x + rate) {
+            return true;
+        }
+
         return false;
     }
 
-    private static boolean check(int[] queenRow, int size) {
-        for (int i=0; i<size; i++) {
-            for (int ni=i+1; ni<size; ni++) {
-                int j = queenRow[i];
-                int nj = queenRow[ni];
-                if (Math.abs(i - ni) == Math.abs(j - nj)) { // 대각선에 존재하는 경우
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private static boolean checkNQueen(int i, int j) {
-        int[] dis = {-1, 1, 1, -1};
-        int[] djs = {1, 1, -1, -1};
-
-        for (int dist = 1; dist <= N; dist++) {
-            for (int k=0; k<4; k++) {
-                int ni = i + dis[k] * dist;
-                int nj = j + djs[k] * dist;
-
-                if (inRange(ni, nj)) {
-                    if (board[ni][nj] == 1) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-
-    private static boolean inRange(int i, int j) {
-        return 0 <= i && i < N && 0 <= j && j < N;
+    static boolean inRange(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < N;
     }
 }
