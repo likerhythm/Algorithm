6e5c59a35e57468bd7bceeb755324bacc3820fd8
Choi Jeongmin <wjdalsdl0412@gmail.com>
2025-10-28 15:59:55 +0900

    Update 그림 교환.java

--- 백준/Gold/1029. 그림 교환/그림 교환.java
+++ 백준/Gold/1029. 그림 교환/그림 교환.java
@@ -1,51 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
+/**
+ * tsp 알고리즘의 dp 테이블이 가지는 값의 의미
+ * => “현재 i번 도시에 있고, 방문 상태가 j일 때, 앞으로 남은 모든 노드를 방문하고 출발지로 돌아가는 경로 중 총 비용이 최소인 경우의 비용”
+ */
 public class Main {
 
     static int N;
     static int[][] arr;
-    static boolean[][][] dp;
+    static boolean[][][] dp; // dp[status][seller][buyer]: 지금까지 그림을 소유했던 아티스트 목록이 status(bit masking)일 때 seller가 buyer에게 그림을 파는 상황을 겪은 유무
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        arr = new int[N + 1][N + 1];
+        arr = new int[N][N];
         dp = new boolean[1 << N][N + 1][N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            String line = br.readLine();
-            for (int j = 1; j <= N; j++) {
-                arr[i][j] = line.charAt(j - 1) - '0';
-            }
+        for (int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
         }
 
-        int BIT = 1 << 0; 
-        int maxPeople = getDP(1, 1, BIT, 0);
-        System.out.println(maxPeople);
+        int status = 1; // 그림을 1번 아티스트만 소유했던 상태. 즉 초기 상태
+        System.out.println(sell(1, status, 0));
     }
 
-    // idx: 현재 아티스트, people: 지금까지 소유한 사람 수
-    // bit: 현재까지 그림을 소유한 사람 집합, num: 이전 거래 가격
-    static int getDP(int idx, int people, int bit, int num) {
-        int maxPeople = people;
+    /**
+     *
+     * @param seller - 현재 그림을 파는 아티스트 번호
+     * @param status - 이때까지 그림을 소유했던 아티스트 목록(bit masking)
+     * @param nowCost - 그림의 직전 거래 가격
+     * @return
+     */
+    private static int sell(int seller, int status, int nowCost) {
+        int count = Integer.bitCount(status);
+        for (int buyer = 1; buyer <= N; buyer++) {
+            if ((status & (1 << (buyer - 1))) != 0) continue; // 이미 buyer 아티스트에게 팔았던 status인 경우
+            if (arr[seller - 1][buyer - 1] < nowCost) continue; // 현재 buyer 아티스트가 그림을 살 수 없는 경우
 
-        for (int i = 1; i <= N; i++) {
-            // 이미 소유했거나 거래 가격이 이전보다 낮으면 패스
-            if ((bit & (1 << (i - 1))) != 0 || arr[idx][i] < num) continue;
+            int nextStatus = status | (1 << (buyer - 1));
+            if (dp[nextStatus][seller][buyer]) continue; // 현재 상태를 이미 겪은 경우
 
-            int nextBit = bit | (1 << (i - 1));
-            // 현재 상태를 이미 겪은 경우 패스
-            if (dp[nextBit][idx][i]) continue;
-            
-            dp[nextBit][idx][i] = true;
-
-            // 재귀 호출 후 최대 사람 수 갱신
-            maxPeople = Math.max(maxPeople, getDP(i, people + 1, nextBit, arr[idx][i]));
+            dp[nextStatus][seller][buyer] = true;
+            count = Math.max(count, sell(buyer, nextStatus, arr[seller - 1][buyer - 1]));
         }
-
-        return maxPeople;
+        return count;
     }
 }
