264f0c46d0998883c7b7d924ceb3ed6df916ca0c
Choi Jeongmin <wjdalsdl0412@gmail.com>
2025-10-28 15:41:04 +0900

    [Gold I] Title: 그림 교환, Time: 284 ms, Memory: 58548 KB -BaekjoonHub

---
+++ 백준/Gold/1029. 그림 교환/그림 교환.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N;
+    static int[][] arr;
+    static int[][][] dp;
+    static final int INF = 200;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1][N + 1];
+        dp = new int[1 << N][N + 1][N + 1];
+
+        for (int i = 0; i < (1 << N); i++) {
+            for (int j = 0; j <= N; j++) {
+                for (int k = 0; k <= N; k++) {
+                    if (j != k) dp[i][j][k] = INF;
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = line.charAt(j - 1) - '0';
+            }
+        }
+
+        int BIT = 1 << 0; 
+        int maxPeople = getDP(1, 1, BIT, 0);
+        System.out.println(maxPeople);
+    }
+
+    // idx: 현재 아티스트, people: 지금까지 소유한 사람 수
+    // bit: 현재까지 그림을 소유한 사람 집합, num: 이전 거래 가격
+    static int getDP(int idx, int people, int bit, int num) {
+        int maxPeople = people;
+
+        for (int i = 1; i <= N; i++) {
+            // 이미 소유했거나 거래 가격이 이전보다 낮으면 패스
+            if ((bit & (1 << (i - 1))) != 0 || arr[idx][i] < num) continue;
+
+            int nextBit = bit | (1 << (i - 1));
+
+            // 이미 더 작은 가격으로 방문했으면 가지치기
+            if (dp[nextBit][idx][i] <= arr[idx][i]) continue;
+
+            dp[nextBit][idx][i] = arr[idx][i];
+
+            // 재귀 호출 후 최대 사람 수 갱신
+            maxPeople = Math.max(maxPeople, getDP(i, people + 1, nextBit, arr[idx][i]));
+        }
+
+        return maxPeople;
+    }
+}
