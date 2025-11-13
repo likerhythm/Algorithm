5c01f1142b798fb007bc9d620dd8ec8360b79b96
Choi Jeongmin <wjdalsdl0412@gmail.com>
2025-11-13 13:02:34 +0900

    [Gold V] Title: 연속합 2, Time: 280 ms, Memory: 27456 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -7,22 +7,31 @@ public class Main {
 
     static int N;
     static int[] arr;
-    static int[][] dp; // dp[i][j] -> if j == 0: i번째 수까지 더할 때 수를 제거한 적이 없는 경우 최대합
-    // if j == 1: i번째 수까지 더할 때 수를 제거한 적이 있는 경우 최대합
+    static int[] dp1, dp2;
+    // dp1[i]: 0번째 수부터 i번째 수까지의 최대합을 구한 dp
+    // dp2[i]: N - 1번째 수부터 i번째 수까지의 최대합을 구한 dp
+    // 실제 최대합은 dp1[i] + dp2[i + 2]를 순환하며 구한다.
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        dp = new int[N][2];
-        dp[0][0] = arr[0];
-        dp[0][1] = 0;
-        int answer = dp[0][0];
+        dp1 = new int[N];
+        dp2 = new int[N];
+
+        dp1[0] = arr[0];
+        int answer = dp1[0];
         for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.max(arr[i], dp[i - 1][0] + arr[i]);
-            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + arr[i]);
-            answer = Math.max(answer, Math.max(dp[i][0], dp[i][1]));
+            dp1[i] = Math.max(dp1[i - 1] + arr[i], arr[i]);
+            answer = Math.max(answer, dp1[i]);
+        }
+        dp2[N - 1] = arr[N - 1];
+        for (int i = N - 2; i >= 0; i--) {
+            dp2[i] = Math.max(dp2[i + 1] + arr[i], arr[i]);
+        }
+        for (int i = 1; i < N - 1; i++) { // i번째 수를 제거했을 때 최대합 구하기
+            answer = Math.max(answer, dp1[i - 1] + dp2[i + 1]);
         }
 
         System.out.println(answer);
