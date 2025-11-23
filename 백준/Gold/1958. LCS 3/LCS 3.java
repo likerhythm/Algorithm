001d8e9cf1fd6c56f6cdc457ec8a1dc2f91d8f81
Choi Jeongmin <wjdalsdl0412@gmail.com>
2025-11-23 11:35:41 +0900

    [Gold IV] Title: LCS 3, Time: 160 ms, Memory: 20268 KB -BaekjoonHub

---
+++ 백준/Gold/1958. LCS 3/LCS 3.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    static int[][][] dp;
+    static String s1, s2, s3;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        s1 = br.readLine();
+        s2 = br.readLine();
+        s3 = br.readLine();
+
+        dp = new int[s1.length() + 1][s2.length() + 1][s3.length() + 1];
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                for (int k = 1; k <= s3.length(); k++) {
+                    if (s1.charAt(i - 1) == s2.charAt(j - 1) && s1.charAt(i - 1) == s3.charAt(k - 1)) {
+                        dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+                    } else {
+                        dp[i][j][k] = Math.max(dp[i - 1][j][k], Math.max(dp[i][j - 1][k], dp[i][j][k - 1]));
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[s1.length()][s2.length()][s3.length()]);
+    }
+}
