f78351f5787d3efce7618e77578b5568f4e4e3e5
Jeongmin Choi <wjdalsdl0412@gmail.com>
2026-05-12 15:25:20 +0900

    [level 2] Title: 프로세스, Time: 1.55 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,33 +1,40 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] priorities, int location) {
-        Deque<Integer> processQueue = new ArrayDeque<>();
-        Deque<Integer> priority = new ArrayDeque<>();
-        List<Integer> answer = new ArrayList<>();
+    
+    class Node {
+        int num;
+        int pri;
         
-        for (int i=0; i<priorities.length; i++) {
-            processQueue.addLast(i);
-            priority.addLast(priorities[i]);
+        public Node(int num, int pri) {
+            this.num = num;
+            this.pri = pri;
+        }
+    }
+    
+    public int solution(int[] priorities, int location) {
+        Queue<Node> q = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(new Node(i, priorities[i]));
         }
         
-        while (!processQueue.isEmpty()) {
-            int popPriority = priority.pop();
-            int popProcess = processQueue.pop();
-            boolean reAdd = false;
-            for (int pr : priority) {
-                if (popPriority < pr) {
-                    priority.addLast(popPriority);
-                    processQueue.addLast(popProcess);
-                    reAdd = true;
+        int count = 0;
+        while (true) {
+            Node poll = q.poll();
+            boolean found = false;
+            for (Node other : q) {
+                if (poll.pri < other.pri) {
+                    q.add(poll);
+                    found = true;
                     break;
                 }
             }
-            if (!reAdd) {
-                answer.add(popProcess);
+            
+            if (found) continue;
+            count++;
+            if (poll.num == location) {
+                return count;
             }
         }
-        
-        return answer.indexOf(location) + 1;
     }
 }
