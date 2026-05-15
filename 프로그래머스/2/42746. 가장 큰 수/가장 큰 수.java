de0a6b2f65639b865ee2b61f5bb93ade898624ed
Jeongmin Choi <wjdalsdl0412@gmail.com>
2026-05-15 19:50:48 +0900

    [level 2] Title: 가장 큰 수, Time: 94.21 ms, Memory: 99.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    
+    public String solution(int[] numbers) {
+        String[] strArr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strArr[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(strArr, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+        
+        if (strArr[0].equals("0")) return "0";
+        StringBuilder sb = new StringBuilder();
+        for (String s : strArr) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
